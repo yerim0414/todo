05fc58cfd8e13da7e8094356f1cf7741818ac2b2
yerim0414 <yerim@withnetworks.com>
2022-03-29 09:44:30 +0900

    db연결확인, 카드생성기능

---
+++ client/src/views/Main.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div v-for="(val) in values">
+            <div>{{val.title}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import draggable from 'vuedraggable'
+    import * as todoApi from '@/api/todo'
+
+    export default {
+        data(){
+            return{
+                values :[{
+                    rows:[]
+                }]
+            }
+        },
+        methods: {
+            getDataLoad(){
+                console.log("start")
+                this.values = []
+                todoApi.getAllList().then(res =>{
+                    // console.log(res.data);
+                    res.data.forEach(element =>{
+
+                        console.log(element);
+                        this.values.push(element);
+                    }
+                    )
+                });
+            },
+        },
+        created(){
+            this.getDataLoad();
+        }
+    }
+
+
+</script>
